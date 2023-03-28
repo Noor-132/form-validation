dd93eff5724f9fb5259007c44d5aa3a735c93d09
Noor-132 <122982711+Noor-132@users.noreply.github.com>
2023-03-29 03:20:38 +0500

    Update script.js

--- form validator/script.js
+++ form validator/script.js
@@ -1,13 +1,44 @@
+const url = 'https://example.com/api/new';
+const formElement = document.querySelector('#myForm');
+
+formElement.addEventListener('submit', function(event) {
+  event.preventDefault();
+  var jsonData = formToJson(this);
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: jsonData
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+});
+
+function formToJson(form) {
+  var formData = new FormData(form);
+  var obj = {};
+  for (var key of formData.keys()) {
+    obj[key] = formData.get(key);
+  }
+  return JSON.stringify(obj);
+}
+
+
 function validateForm() {
   const email = document.forms["myForm"]["email"].value;
   const fname = document.forms["myForm"]["fname"].value;
   const lname = document.forms["myForm"]["lname"].value;
   const city = document.forms["myForm"]["city"].value;
-  const area = document.forms["myForm"]["area"].value;
+  const adress = document.forms["myForm"]["adress"].value;
   const payment = document.forms["myForm"]["payment"].checked;
   const agreement = document.forms["myForm"]["agreement"].checked;
 
-  if (email === "" || fname === "" || lname === "" || number === "" || payment === false) {
+    
+
+  if (email === "" || fname === "" || lname === "" ||adress === "" ||code === "" ||instruction === "" || payment === false) {
     alert("Please fill in all required fields.");
     return false;
   }
@@ -18,13 +49,13 @@ function validateForm() {
     return false;
   }
 
-  const numberRegex = /^[0-9]+$/;
-  if (!numberRegex.test(number)) {
-    alert("Please enter a valid phone number.");
-    return false;
-  }
+  // const numberRegex = /^[0-9]+$/;
+  // if (!numberRegex.test(number)) {
+  //   alert("Please enter a valid phone number.");
+  //   return false;
+  // }
 
-  if (country === "" || province === "" || city === "") {
+  if (country === "" || city === "") {
     alert("Please fill in all required fields.");
     return false;
   }
@@ -36,3 +67,14 @@ function validateForm() {
 
   return true;
 }
+
+
+
+
+
+
+
+
+
+
+
